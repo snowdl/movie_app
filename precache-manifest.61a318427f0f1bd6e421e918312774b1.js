dea3dca770f5208ff508f2839adc0f4fa428912e
snowdl <snowdl@naver.com>
2020-03-30 10:56:01 +0900

    Updates

---
+++ precache-manifest.61a318427f0f1bd6e421e918312774b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9dbc582d4e38d6e609a9eb748b4ce5fb",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "e519529039ec2e3b42ba",
+    "url": "/movie_app/static/css/main.0ba14af7.chunk.css"
+  },
+  {
+    "revision": "3c706595e99a154795c5",
+    "url": "/movie_app/static/js/2.4e522206.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.4e522206.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e519529039ec2e3b42ba",
+    "url": "/movie_app/static/js/main.0337845e.chunk.js"
+  },
+  {
+    "revision": "ca4b155c3570fabe1dd4",
+    "url": "/movie_app/static/js/runtime-main.98d66c69.js"
+  }
+]);
